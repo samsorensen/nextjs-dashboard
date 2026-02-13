b5c5c1d99899350e40c18458a86024f05ac9ca0f
samsorensen <143055587+samsorensen@users.noreply.github.com>
2026-02-13 13:18:07 -0700

    Add Auth + Login Page

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -2,7 +2,7 @@
 
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { deleteInvoice } from '@/app/lib/action';
+import { deleteInvoice } from '@/app/lib/actions';
 import { useFormStatus } from 'react-dom';
 
 export function CreateInvoice() {
